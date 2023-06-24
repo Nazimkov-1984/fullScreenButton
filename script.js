60da5434d04ff0e54d341554e14688fddc318a99
Mykola Nazimkov <nazimkoff@gmail.com>
2023-06-24 12:48:55 +0300

    add color

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-
 document.querySelector("button")?.addEventListener("click", () => {
-  document.documentElement.requestFullscreen();
-});
+  document.documentElement.requestFullscreen()
+  document.querySelector("button").style.backgroundColor = "green"
+})
+document.addEventListener("keydown", (e) => {
+  if (e.keyCode === 27) {
+    document.querySelector("button").style.backgroundColor = "grey"
+  }
+})
